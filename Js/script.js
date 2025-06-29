c59bc32d47b7062ba0e332ad76f4fdd6cdbe66fe
Elsurraj <surrajabubakhar@gmail.com>
2025-06-28 23:50:10 -0700

    Fix script.js typo and update script references

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,14 @@
 
 //js for handbugger Menu
-var menuItems=document.getElementById("menuItems");
- menuItems.style.maxHeight='0px';
 
-function menutoggle(){
-    if(menuItems.style.maxHeight=='0px'){
-     menuItems.style.maxHeight='200px';
+ function menutoggle() {
+      var menuItems = document.getElementById("menuItems");
+      if (menuItems.style.maxHeight === "0px" || !menuItems.style.maxHeight) {
+        menuItems.style.maxHeight = "200px";
+      } else {
+        menuItems.style.maxHeight = "0px";
+      }
     }
-    else{
-        menuItems.style.maxHeight='0px';
-    }
-}
 
 const year = new Date().getFullYear();
  document.getElementById('currentYear').textContent = year;
